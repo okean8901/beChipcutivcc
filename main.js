3b1764255e2f0f8998a3162ce6c7007b7df7b29d
okean8901 <trangialoc892001@gmail.com>
2024-08-31 16:49:00 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,67 @@
+"use strict";
+
+const titleElement = document.querySelector(".title");
+const buttonsContainer = document.querySelector(".buttons");
+const yesButton = document.querySelector(".btn--yes");
+const noButton = document.querySelector(".btn--no");
+const catImg = document.querySelector(".cat-img");
+
+const MAX_IMAGES = 5;
+
+let play = true;
+let noCount = 0;
+
+yesButton.addEventListener("click", handleYesClick);
+
+noButton.addEventListener("click", handleNoClick);
+
+catImg.addEventListener("click", handleNoClick);
+
+function handleYesClick() {
+  titleElement.innerHTML = "Yayyy!! Tớ iu cậu :3";
+  buttonsContainer.classList.add("hidden");
+  changeImage("yes");
+}
+
+function handleNoClick() {
+  if (play) {
+    noCount++;
+    const imageIndex = Math.min(noCount, MAX_IMAGES);
+    changeImage(imageIndex);
+    resizeYesButton();
+    updateNoButtonText();
+    if (noCount === MAX_IMAGES) {
+      play = false;
+    }
+  }
+}
+
+function resizeYesButton() {
+  const computedStyle = window.getComputedStyle(yesButton);
+  const fontSize = parseFloat(computedStyle.getPropertyValue("font-size"));
+  const newFontSize = fontSize * 1.6;
+
+  yesButton.style.fontSize = `${newFontSize}px`;
+}
+
+function generateMessage(noCount) {
+  const messages = [
+    "No",
+    "Cậu chắc chứ?",
+    "Làm ơn đó :((",
+    "Đừng làm vậy với tớ :(",
+    "Cậu sẽ làm tan nát trái tim tớ",
+    "Tớ sẽ khóc đó...",
+  ];
+
+  const messageIndex = Math.min(noCount, messages.length - 1);
+  return messages[messageIndex];
+}
+
+function changeImage(image) {
+  catImg.src = `img/cat-${image}.jpg`;
+}
+
+function updateNoButtonText() {
+  noButton.innerHTML = generateMessage(noCount);
+}
